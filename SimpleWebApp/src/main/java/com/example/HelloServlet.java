aca4f60ca1056621f8bb36e8706cf75a04b87eee
Chetan <chetanvshah20@gmail.com>
2025-04-30 23:36:37 +0530

    Initial Commit

---
+++ SimpleWebApp/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.io.*;
+import java.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet{
+   protected void doGet(HttpServletRequest request,HttpServletResponse response)
+	   throws ServletException,IOException{
+   response.setContentType("text/html")
+   PrintWriter out = response.getWriter();
+   out.println("<h1>Hello from Servlet</h1>");
+
+
+   }
+
+
+}
+
