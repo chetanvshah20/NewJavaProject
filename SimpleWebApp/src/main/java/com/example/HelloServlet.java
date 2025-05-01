47941a5df4484b89280456b51243cd803747c085
Chetan V. Shah <79932411+chetanvshah20@users.noreply.github.com>
2025-05-01 09:37:03 +0530

    Update HelloServlet.java

--- SimpleWebApp/src/main/java/com/example/HelloServlet.java
+++ SimpleWebApp/src/main/java/com/example/HelloServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.*;
 public class HelloServlet extends HttpServlet{
    protected void doGet(HttpServletRequest request,HttpServletResponse response)
 	   throws ServletException,IOException{
-   response.setContentType("text/html")
+   response.setContentType("text/html");
    PrintWriter out = response.getWriter();
    out.println("<h1>Hello from Servlet</h1>");
 
